4070e1dd1fc6467db1caf1257f062faa28610768
asif26679 <asif26679@gmail.com>
2025-09-11 14:50:14 +0530

    me

--- src/components/DashboardContent.jsx
+++ src/components/DashboardContent.jsx
@@ -41,11 +41,7 @@ export default function Dashboard() {
         const token = localStorage.getItem("token");
         if (!token) return navigate("/login");
 
-        const resUser = await api.get("/users/me", {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        setUser(resUser.data.user); 
-
+        
         const res = await api.get("/diets", {
           headers: { Authorization: `Bearer ${token}` },
         });
