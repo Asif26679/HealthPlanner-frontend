9f523202a6291547b9336e00b0a5d539cffc8a24
asif26679 <asif26679@gmail.com>
2025-09-12 20:53:17 +0530

    e

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function SkeletonCard() {
   return (
-    <div className="bg-gray-800/50 rounded-xl shadow-md p-4 animate-pulse h-40 w-full">
-      <div className="h-6 w-24 bg-gray-600 rounded mb-4"></div>
-      <div className="h-4 w-16 bg-gray-600 rounded mb-2"></div>
-      <div className="h-4 w-32 bg-gray-600 rounded"></div>
+    <div className="bg-red-600 rounded-xl shadow-md p-4 animate-pulse h-40 w-full">
+      <div className="h-6 w-24 bg-green-400 rounded mb-4"></div>
+      <div className="h-4 w-16 bg-green-600 rounded mb-2"></div>
+      <div className="h-4 w-32 bg-green-600 rounded"></div>
     </div>
   );
 }
