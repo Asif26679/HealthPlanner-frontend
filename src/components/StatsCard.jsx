565d449f0e10cdd67d9c05f5094312feef6f8794
asif26679 <asif26679@gmail.com>
2025-09-12 21:13:41 +0530

    me

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -7,7 +7,7 @@ export default function StatsCard({ title, value, icon: Icon, gradient, onClick 
       className={`p-5 ${gradient} rounded-xl shadow-lg flex flex-col items-center justify-center gap-2 cursor-pointer hover:scale-105 transform transition-all duration-300`}
     >
       {Icon && <Icon size={28} />}
-      <p className="text-sm text-gray-900">{title}</p>
+      <p className="text-sm text-white">{title}</p>
       {value && <h2 className="text-xl font-bold">{value}</h2>}
     </div>
   );
